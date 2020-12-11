4a44a630232698189c1012c8446ace376b5be305
Julen Ortiz <5983309+Julortiz@users.noreply.github.com>
2020-12-11 19:49:54 +0100

    Iruzkina lerrokatu

--- definetg.h
+++ definetg.h
@@ -6,11 +6,11 @@
 
 // konstanteak
 
-#define EMAX     230000        // osagai kopuru (elem) maximoa
+#define EMAX     230000     // osagai kopuru (elem) maximoa
 #define TALDEKOP 100        // talde kopurua
-#define ALDAKOP  40        // elementu bakoitzaren aldagai kopurua
-#define DELTA    0.01        // konbergentzia: zentroideen aldaketa minimoa
-#define ITMAX    1000        // konbergentzia: iterazio kopuru maximoa
+#define ALDAKOP  40         // elementu bakoitzaren aldagai kopurua
+#define DELTA    0.01       // konbergentzia: zentroideen aldaketa minimoa
+#define ITMAX    1000       // konbergentzia: iterazio kopuru maximoa
 #define ERIMOTA  20
 
 
@@ -18,14 +18,14 @@
 
 struct tinfo            // taldei buruzko informazioa
 {
-    int osagaiak[EMAX];        // osagaiak
+    int osagaiak[EMAX]; // osagaiak
     int kop;            // osagai kopurua
 };
 
 
-struct analisia        // eritasunen analisiaren emaitzak
+struct analisia         // eritasunen analisiaren emaitzak
 {
-    float max, min;                // maximoa eta minimoa eritasun bakoitzerako
-    int tmax, tmin;            // maximoko eta minimoko taldeak
+    float max, min;     // maximoa eta minimoa eritasun bakoitzerako
+    int tmax, tmin;     // maximoko eta minimoko taldeak
 };
 
